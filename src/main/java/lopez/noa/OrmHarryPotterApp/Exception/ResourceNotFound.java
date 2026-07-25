81621e1ca3e39fb4e4b2c31566d60a5e032b90b0
Noa <noaloprg@gmail.com>
2026-07-25 18:04:51 +0200

    refactor(ResourceNotFOund): elimina metodo repetido y añade doc

--- src/main/java/lopez/noa/OrmHarryPotterApp/Exception/ResourceNotFound.java
+++ src/main/java/lopez/noa/OrmHarryPotterApp/Exception/ResourceNotFound.java
@@ -2,22 +2,24 @@ package lopez.noa.OrmHarryPotterApp.Exception;
 
 import java.math.BigInteger;
 
-// Excepcion para cuando un recurso no existe o no se ha encontrado
+/**
+ * Excepcion para cuando un recurso no existe o no se ha encontrado
+ */
 public class ResourceNotFound extends RuntimeException {
     private static final String MENSAJE_ERROR = "No se encontro";
 
-    //POR DEFECTO
     public ResourceNotFound(String message) {
         super(message);
     }
+
     public ResourceNotFound(String mensaje, Throwable lanzable) {
         super(mensaje, lanzable);
     }
-
-    //MENSAJE PERSONALIZADO SEGUN ID
-    public ResourceNotFound(Integer id, String entidad) {
-        super(String.format("%s %s con ID %d", MENSAJE_ERROR, entidad, id));
-    }
+    
+    /**
+     * @param id      id de la entidad que no se ha encontrado
+     * @param entidad nombre de la entidad que se mostrara en el mensaje, para poder retulizar
+     */
     public ResourceNotFound(BigInteger id, String entidad) {
         super(String.format("%s %s con ID %d", MENSAJE_ERROR, entidad, id));
     }
